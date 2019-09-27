33aea8ef29c3e60f78e9634fd4f7081f723050f4
Phu D. Nguyen <Phu.Nguyen@sandata.com>
2019-09-27 15:49:54 +0700

    remove unused structs

--- model/Face.go
+++ model/Face.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"github.com/globalsign/mgo/bson"
-	"image"
 )
 
 type Face struct {
@@ -12,25 +11,4 @@ type Face struct {
 	MD5        string        `json:"md5" bson:"md5"`
 	DeviceId   string        `json:"deviceId" bson:"deviceId"`
 	ProjectId  string        `json:"projectId" bson:"projectId"`
-}
-
-type RecognizeResponse struct {
-	RecognizedFaces []RecognizedFace `json:"recognizedFaces"`
-	Image           string           `json:"image"`
-}
-
-type RecognizedFace struct {
-	Rect       image.Rectangle `json:"rect"`
-	Label      string          `json:"label"`
-	Classified int             `json:"category"`
-}
-
-type DetectResponse struct {
-	DetectedFaces []DetectedFace `json:"detectedFaces"`
-	Image         string         `json:"image"`
-}
-
-type DetectedFace struct {
-	Rect       image.Rectangle `json:"rect"`
-	Descriptor [128]float32    `json:"descriptor"`
 }
