6194eac99b8dbe5712beb0e2d1c78f584fe73e9b
Phu D. Nguyen <Phu.Nguyen@sandata.com>
2019-10-06 16:45:15 +0700

    added new notification type

--- model/Notification.go
+++ model/Notification.go
@@ -10,16 +10,21 @@ var (
 	NotificationSeverityWarning = "warning"
 	NotificationSeverityDanger  = "danger"
 
-	NotificationTypeSlack = "SLACK"
+	NotificationTypeSittingRemind     = "SITTING_REMIND"
+	NotificationTypeDeviceStatusAlert = "DEVICE_STATUS_ALERT"
 )
 
 type Notification struct {
-	Id              bson.ObjectId `json:"id" bson:"_id"`
-	Type            string        `json:"type" bson:"type"`
-	DeskId          string        `json:"deskId" bson:"deskId"`
-	DeviceId        string        `json:"deviceId" bson:"deviceId"`
-	Timestamp       time.Time     `json:"timestamp" bson:"timestamp"`
-	UserId          bson.ObjectId `json:"owner" bson:"owner"`
-	SitDuration     time.Duration `json:"sitDuration" bson:"sitDuration"`
-	Rule            Rule          `json:"rule" bson:"rule"`
+	Id        bson.ObjectId `json:"id" bson:"_id"`
+	Type      string        `json:"type" bson:"type"`
+	DeskId    string        `json:"deskId" bson:"deskId"`
+	DeviceId  string        `json:"deviceId" bson:"deviceId"`
+	Timestamp time.Time     `json:"timestamp" bson:"timestamp"`
+	UserId    bson.ObjectId `json:"owner" bson:"owner"`
+	// for sitting only
+	SitDuration time.Duration `json:"sitDuration" bson:"sitDuration"`
+	Rule        Rule          `json:"rule" bson:"rule"`
+
+	// for device
+	OfflineSince time.Time `json:"offlineSince" bson:"offlineSince"`
 }
